da94722a51bd147a498b5716065282118597a272
darquinius <james@darquini.us>
2022-02-09 19:36:25 +0200

    to be removed

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+    // Get today's date in ISO format
+    var today = new Date();
+    var date = today.toISOString().slice(0, -14);
+    console.log(date);
+
+    $('.home-wrap').append('<p>' + date + '</p>');
+
+
+    $.getJSON('http://crystalhoroscope.com/wp-content/themes/magic-quiz/content/dailyhoroscope/dhe.json');
+
+
+});
+
+    
